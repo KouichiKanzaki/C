3d7e602f4ddf5e4dfeaaa7ada53d0b192d821f3b
kouichi <likfsm4@gmail.com>
2022-12-19 07:46:27 +0900

    C

---
+++ TEST/TEST/TEST.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+struct Data_tag {
+    int a;
+    double b;
+    char c[4];
+    struct Data_tag* d;
+};
+
+void print_data(const struct Data_tag* data)
+{
+    printf(".a == %d\n", data->a);
+    printf(".b == %f\n", data->b);
+    printf(".c == %s\n", data->c);
+    printf(".d == %p\n", data->d);
+}
+
+int main(void)
+{
+    struct Data_tag s;
+
+    // s のすべてのメンバに 0 を入れる
+
+    print_data(&s);
+}
